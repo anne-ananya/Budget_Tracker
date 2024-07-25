b32c620574546c0a1a4b52e1739e2234087dfd48
Ali Raza <alirazaqasim192@gmail.com>
2024-07-25 15:58:18 +0500

    manage page step complete

--- app/(dashborad)/_components/CreateCategoryDialog.tsx
+++ app/(dashborad)/_components/CreateCategoryDialog.tsx
@@ -31,7 +31,7 @@ import { TransactionType } from "@/lib/types";
 import { cn } from "@/lib/utils";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { CircleOff, Loader2, PlusSquare } from "lucide-react";
-import React, { useCallback } from "react";
+import React, { ReactNode, useCallback } from "react";
 import { useForm } from "react-hook-form";
 import Picker from "@emoji-mart/react";
 import data from "@emoji-mart/data";
@@ -44,9 +44,10 @@ import { useTheme } from "next-themes";
 interface Props {
   type: TransactionType;
   successCallback: (category: Category) => void;
+  trigger?: ReactNode;
 }
 
-function CreateCategoryDialog({ type, successCallback }: Props) {
+function CreateCategoryDialog({ type, successCallback, trigger }: Props) {
   const [open, setOpen] = React.useState(false);
 
   const form = useForm<CreateCategorySchemaType>({
@@ -101,7 +102,7 @@ function CreateCategoryDialog({ type, successCallback }: Props) {
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>
-        <Button
+        {trigger ? (trigger) : (<Button
           variant={"ghost"}
           className="flex border-separate items-center justify-start
           rounded-none border-b px-3 py-3 text-muted-foreground"
@@ -109,6 +110,7 @@ function CreateCategoryDialog({ type, successCallback }: Props) {
           <PlusSquare className="mr-2 h-4 w-4" />
           Create new
         </Button>
+        )}
       </DialogTrigger>
       <DialogContent>
         <DialogHeader>
